57d9ac0f47cc61a6802b2610444c901c050ec8fb
Kholeif <36528209+Kholeif@users.noreply.github.com>
2019-03-17 17:45:25 +0200

    Add files via upload

---
+++ MCAL Driver_functions/DIO_Write_Port.c
@@ -0,0 +1,49 @@
+#include "PORT_Driver.h"
+
+void DIO_WritePort(uint8_t port_index,uint8_t pins_mask,Dio_LevelType pins_level)
+{
+	switch(port_index)
+	{
+		case'0':
+			if(pins_level)
+				GPIO_PORTA_DATA_R |= pins_mask;
+			else
+				GPIO_PORTA_DATA_R &= ~pins_mask;
+			break;
+	
+		case'1':
+			if(pins_level)
+				GPIO_PORTB_DATA_R |= pins_mask;
+			else
+				GPIO_PORTB_DATA_R &= ~pins_mask;
+			break;
+			
+		case'2':
+			if(pins_level)
+				GPIO_PORTC_DATA_R |= pins_mask;
+			else
+				GPIO_PORTC_DATA_R &= ~pins_mask;
+			break;
+			
+		case'3':
+			if(pins_level)
+				GPIO_PORTD_DATA_R |= pins_mask;
+			else
+				GPIO_PORTD_DATA_R &= ~pins_mask;
+			break;
+			
+		case'4':
+			if(pins_level)
+				GPIO_PORTE_DATA_R |= pins_mask;
+			else
+				GPIO_PORTE_DATA_R &= ~pins_mask;
+			break;
+			
+		case'5':
+			if(pins_level)
+				GPIO_PORTF_DATA_R |= pins_mask;
+			else
+				GPIO_PORTF_DATA_R &= ~pins_mask;
+			break;
+	}
+}
