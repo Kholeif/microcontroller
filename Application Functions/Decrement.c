b5a65bdc6e2593af3eb971c84f1f4a0c0ddc3130
Kholeif <36528209+Kholeif@users.noreply.github.com>
2019-03-18 16:49:00 +0200

    Add files via upload

---
+++ Application Functions/Decrement.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include "C:/Keil/EE319Kware/PROJECT I/PORT_Driver.h"
+#include "C:/Keil/EE319Kware/PROJECT I/tm4c123gh6pm.h"
+#include "C:/Keil/EE319Kware/PROJECT I/lcd.h"
+
+int units = 0 ;
+int tens = 0 ; 
+int hundreds = 0 ;
+
+
+void Decrement (uint8_t port_index,uint8_t pins_mask)
+{
+		
+	  int input = units + 10 * tens + 100 * hundreds ; 
+    while ( DIO_Read_Port( port_index,pins_mask) == 1 ) 
+		{
+    input -- ;
+    int spare = input ;
+	  units =  spare  %10;
+    spare = spare / 10 ; 
+    tens = spare % 10 ;
+    spare = spare / 10 ;
+    hundreds = spare % 10 ; 
+    }	
+} 
