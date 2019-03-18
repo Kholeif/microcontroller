d3b39f2b2a02c5b29a5689a567f607f3e45be0e5
MohamedYaman <36310797+MohamedYaman@users.noreply.github.com>
2019-03-18 17:03:14 +0200

    Update Increment.c

--- Application Functions/Increment.c
+++ Application Functions/Increment.c
@@ -3,16 +3,14 @@
 #include "C:/Keil/EE319Kware/PROJECT I/tm4c123gh6pm.h"
 #include "C:/Keil/EE319Kware/PROJECT I/lcd.h"
 
-void Increment (uint8_t port_index,uint8_t pins_mask){
+void Increment (void){
 		
-	int input = units + 10 * tens + 100 * hundreds ; 
-	while ( DIO_Read_Port( port_index,pins_mask) == 1 ) {
-			input ++ ;
-			int spare = input ;
-				units =  spare  %10;
-			spare = spare / 10 ; 
-			tens = spare % 10 ;
-			spare = spare / 10 ;
-			hundreds = spare % 10 ; 
-	}	
+	int input = units + 10 * tens + 100 * hundreds ;
+	input ++ ;
+	int spare = input ;
+	units =  spare  %10;
+	spare = spare / 10 ; 
+	tens = spare % 10 ;
+	spare = spare / 10 ;
+	hundreds = spare % 10 ; 	
 } 
